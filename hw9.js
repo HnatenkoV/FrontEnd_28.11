c2210ae0ab1310e7ac0a2e67626952c2245b685f
Hnatenkov <hnatenko.vadim3@gmail.com>
2023-01-07 15:19:27 +0200

    lesson9

--- hw9.js
+++ hw9.js
@@ -3,16 +3,19 @@ const contentDiv = document.getElementById('contentDiv');
 const btnEnter = document.getElementById('btnEnter');
 const btnClear = document.getElementById('btnClear');
 
-btnEnter.addEventListener('click', () => {
+function clickBtnEnter () {
     if (input.value === '') {
         input.placeholder = "Заполните это поле";
     }
 
     contentDiv.textContent = input.value;
+}
 
-});
+btnEnter.addEventListener('click', clickBtnEnter );
 
-btnClear.addEventListener("click", () => {
+function clickBtnClear () {
     input.value = '';
     contentDiv.textContent = '';
-});
+}
+
+btnClear.addEventListener('click', clickBtnClear);
